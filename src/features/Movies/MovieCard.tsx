0dc772c9aa216a7b3f81792fa5f7872794bc8383
d42null <d2null@gmail.com>
2024-04-13 15:02:33 +0300

    1class

---
+++ src/features/Movies/MovieCard.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import  "./Movies.css"
+
+
+interface MovieCardProps{
+    id:number;
+    title:string;overview:string;popularity:number;
+}
+export function MovieCard ({id,title,overview,popularity}:MovieCardProps){
+    return( <div  className="movies-card">
+        <Link to={`/movies/${id}`}>{title}</Link>
+        <div className="movies-card-overview">{overview}</div>
+        <div className="movies-card-pop">{popularity}</div>
+      </div>)
+}
