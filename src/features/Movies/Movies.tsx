c62216dab88fba7c66a87ab447bea4f77e547ffd
d42null <d2null@gmail.com>
2024-04-19 17:49:52 +0300

    10-11. RTK Query demo

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,41 +1,57 @@
-import { Suspense, lazy, useCallback, useContext, useEffect, useState } from "react";
-import { fetchNextPage, resetMovies } from "../../reducers/movies";
-import { useAppDispatch, useAppSelector } from "../../hooks";
+import { Suspense, lazy, useCallback, useContext,  useState } from "react";
+// import { fetchNextPage, resetMovies } from "../../reducers/movies";
+// import { useAppDispatch, useAppSelector } from "../../hooks";
 import { Container, Grid, LinearProgress, Typography } from "@mui/material";
 import { AuthContext, anonymousUser } from "../../AuthContext";
 import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
 // import { Filters } from "./MoviesFilter";
 import MovieCard from "./MovieCard";
- 
+import { MovieDetails, MoviesFilters, MoviesQuery, useGetConfigurationQuery, useGetMoviesQuery } from "../../services/tmdb";
+const initialQuery:MoviesQuery = {
+  page: 1,
+  filters: {},
+};
 const MoviesFilter=lazy(()=>import("./MoviesFilter"))
 export default function Movies() {
-  const dispatch = useAppDispatch();
-  const movies = useAppSelector((state) => state.movies.top);
-  const loading = useAppSelector((state) => state.movies.loading);
-  const hasMorePages = useAppSelector((state) => state.movies.hasMorePages);
-
-  const [filters, setFilters] = useState<any>();
+  const [query, setQuery] = useState<MoviesQuery>(initialQuery);
+  const { data: configuration } = useGetConfigurationQuery();
+  const { data, isFetching } = useGetMoviesQuery(query);
+  // const dispatch = useAppDispatch();
+  // const movies = useAppSelector((state) => state.movies.top);
+  const movies = data?.results ?? []
+  // const loading = useAppSelector((state) => state.movies.loading);
+  // const hasMorePages = useAppSelector((state) => state.movies.hasMorePages);
+  const hasMorePages = data?.hasMorePages;
+  function formatImageUrl(imagePath?: string | null) {
+    return imagePath && configuration ? `${configuration.images.base_url}w780${imagePath}` : undefined;
+  }
+  // const [filters, setFilters] = useState<any>();
 
   const auth = useContext(AuthContext);
   const loggedIn = auth.user !== anonymousUser;
-  const [targetRef, entry] = useIntersectionObserver();
-
-  useEffect(() => {
-    dispatch(resetMovies());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (entry?.isIntersecting && hasMorePages) {
-      const moviesFilters = filters
-        ? {
-            keywords: filters?.keywords.map((k:any) => k.id),
-            genres: filters?.genres,
-          }
-        : undefined;
-
-      dispatch(fetchNextPage(moviesFilters));
+  const onIntersect = useCallback(() => {
+    if (hasMorePages) {
+      setQuery((q) => ({ ...q, page: q.page + 1 }));
     }
-  }, [dispatch, entry?.isIntersecting, filters, hasMorePages]);
+  }, [hasMorePages]);
+  const [targetRef] = useIntersectionObserver({onIntersect});
+
+  // useEffect(() => {
+  //   dispatch(resetMovies());
+  // }, [dispatch]);
+ 
+  // useEffect(() => {
+  //   if (entry?.isIntersecting && hasMorePages) {
+  //     const moviesFilters = filters
+  //       ? {
+  //           keywords: filters?.keywords.map((k:any) => k.id),
+  //           genres: filters?.genres,
+  //         }
+  //       : undefined;
+
+  //     dispatch(fetchNextPage(moviesFilters));
+  //   }
+  // }, [dispatch, entry?.isIntersecting, filters, hasMorePages]);
 const handleAddToFavorite=useCallback(
   (id: number): void => alert(`Not implemented! Action: ${auth.user.name} is adding movie ${id} to favorites.`),
   [auth.user.name]
@@ -46,17 +62,24 @@ const handleAddToFavorite=useCallback(
         <Suspense fallback={<span>Loading filters...</span>}>
         <MoviesFilter
           onApply={(filters) => {
-            dispatch(resetMovies());
-            setFilters(filters);
+            // dispatch(resetMovies());
+            // setFilters(filters);
+            const moviesFilters:MoviesFilters={
+              keywords:filters.keywords.map(k=>k.id),
+              genres:filters.genres,
+            }
+            setQuery({
+              page:1,filters:moviesFilters
+            })
           }}
         />
         </Suspense>
       </Grid>
       <Grid item xs={12}>
         <Container sx={{ py: 8 }} maxWidth="lg">
-          {!loading && !movies.length && <Typography variant="h6">No movies were found that match your query.</Typography>}
+          {!isFetching && !movies.length && <Typography variant="h6">No movies were found that match your query.</Typography>}
           <Grid container spacing={4}>
-            {movies.map((m, i) => (
+            {movies.map((m:MovieDetails, i:number) => (
               <Grid item key={`${m.id}-${i}`} xs={12} sm={6} md={4}>
                 <MovieCard
                   key={m.id}
@@ -64,14 +87,14 @@ const handleAddToFavorite=useCallback(
                   title={m.title}
                   overview={m.overview}
                   popularity={m.popularity}
-                  image={m.image}
+                  image={formatImageUrl(m.backdrop_path)}
                   enableUserActions={loggedIn}
                   onAddFavorite={handleAddToFavorite}
                 />
               </Grid>
             ))}
           </Grid>
-          <div ref={targetRef}>{loading && <LinearProgress color="secondary" sx={{ mt: 3 }} />}</div>
+          <div ref={targetRef}>{isFetching && <LinearProgress color="secondary" sx={{ mt: 3 }} />}</div>
         </Container>
       </Grid>
     </Grid>
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,22 @@
-import { UnknownAction, applyMiddleware, createStore } from "redux";
-import rootReducer from './reducers';
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { ThunkAction, thunk } from "redux-thunk";
-const composedEnhancer= composeWithDevTools(applyMiddleware(thunk))
-const store= createStore(rootReducer,composedEnhancer);
-export type AppDispatch=typeof store.dispatch;
-export type AppThunk<ReturnType>=ThunkAction<ReturnType,RootState,undefined,UnknownAction>;
-export type RootState=ReturnType<typeof store.getState>;
+import { UnknownAction, } from "redux";
+
+import { ThunkAction} from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import { tmdbApi } from "./services/tmdb";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+// const composedEnhancer= composeWithDevTools(applyMiddleware(thunk))
+// const store= createStore(rootReducer,composedEnhancer);
+const store = configureStore({
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
+});
+setupListeners(store.dispatch);
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType> = ThunkAction<ReturnType, RootState, undefined, UnknownAction>;
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppDispatch=useDispatch<AppDispatch>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export default store;
